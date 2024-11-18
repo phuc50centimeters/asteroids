0c35044d736b6f09932b6460e23b6b2767b3db92
phuc50centimeters <phuc.50centimeters@gmail.com>
2024-11-18 17:41:21 +0700

    Add shooting, asteroid spawing

--- main.py
+++ main.py
@@ -1,20 +1,66 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+import sys
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+
+        # Update
+        for object in updatable:
+            if isinstance(object, Player):
+                object.shot_cooldown -= dt
+
+            object.update(dt)
+
+        # Check collisions
+        for asteroid in asteroids:
+            # Check game over
+            if asteroid.is_colliding(player):
+                print("Game over!")
+                sys.exit()
+
+            # Check bullet
+            for shot in shots:
+                if shot.is_colliding(asteroid):
+                    shot.kill()
+                    asteroid.split()
+
+        # Draw
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
-        pass
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        # Pause the game until 1/60th of a second has passed 
+        # Make sure the game run at 60FPS
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
